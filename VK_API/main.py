6ac65d70ad06a57084ac52b35dc99bd387aebf4c
Jurex <jurexda@yandex.ru>
2023-05-16 00:04:41 +0500

    Done with parsing photos to local directory.

--- VK_API/main.py
+++ VK_API/main.py
@@ -1,3 +1,6 @@
+import os
+
+import requests
 import vk_api
 
 with open("private_token.txt", 'r') as text_file:
@@ -6,8 +9,32 @@ with open("private_token.txt", 'r') as text_file:
 session = vk_api.VkApi(token=tok)
 vk = session.get_api()
 
-MY_ID = 332315239
+MY_STRING_ID = 'm.a_sick'
 COUNT_GROUPS = 20
+
+
+def get_user_photoes(user_id: int) -> dict:
+    photoes = session.method('photos.getAll', {'owner_id': user_id})
+    return photoes
+
+
+def parse_user_photoes(photoes_dict: dict):
+    photos_urls = []
+    items = photoes_dict['items']
+    user_id = items[0]['owner_id']
+    for item in items:
+        photos_urls.append(item['sizes'][0]['url'])
+
+    save_photoes_by_user_name(photos_urls, user_id)
+
+
+def get_user_name_by_id(user_id: int) -> str:
+    user = session.method("users.get", {"user_ids": user_id})  # вместо 1 подставляете айди нужного юзера
+    return user[0]['first_name'] + ' ' + user[0]['last_name']
+
+
+def get_int_id_by_string_id(user_str_id: str) -> int:
+    return session.method("users.get", {'user_ids': user_str_id})[0]['id']
 
 
 def get_groups(user_id: int, count_groups: int):
@@ -27,6 +54,12 @@ def get_friends(user_id: int):
     return friends_ids
 
 
+def get_photo_albums(user_id: int):
+    all_albums = session.method("photos.getAlbums",
+                                {"owner_id": str(user_id)})
+    return all_albums
+
+
 def parse_friends(friends: dict):
     print("Общее количество друзей: " + str(friends['count']))
     items = friends['items']
@@ -44,5 +77,37 @@ def parse_groups(groups: dict):
         print(group['name'])
 
 
-parse_friends(get_friends(MY_ID))
-parse_groups(get_groups(MY_ID, COUNT_GROUPS))
+def parse_photo_albums(albums: dict):
+    print("Альбомы пользователя(название и описания)")
+    count_albums = albums['count']
+    print(f'У пользователя на данный момент {count_albums} альбомов:')
+    for album in albums['items']:
+        print('Название альбома ' + album['title'])
+        print('Описание альбома ' + album['description'])
+        print('___________________________________________')
+
+
+def save_photoes_by_user_name(photoes_urls: list, user_id: int):
+    directory = f"Photos_{get_user_name_by_id(user_id)}"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    count = 0
+    for url in photoes_urls:
+        count += 1
+        img_data = requests.get(url).content
+        file_name = f'picture{count}'
+        with open(f'{directory}/{file_name}.jpg', 'wb') as handler:
+            handler.write(img_data)
+
+
+if __name__ == '__main__':
+    MY_ID = get_int_id_by_string_id(MY_STRING_ID)
+    print(get_user_name_by_id(MY_ID))
+    parse_friends(get_friends(MY_ID))
+    print()
+    print()
+    parse_groups(get_groups(MY_ID, COUNT_GROUPS))
+    print()
+    print()
+    parse_photo_albums(get_photo_albums(MY_ID))
+    parse_user_photoes(get_user_photoes(MY_ID))
